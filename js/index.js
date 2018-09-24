b76312b1f909fdea40c957f5d881bd2b0d998e8a
mieynaudi <mieynaudi@uc.cl>
2018-09-23 23:56:28 -0300

    added notifications

--- js/index.js
+++ js/index.js
@@ -2,40 +2,72 @@ app = {
     "data": []
 }
 
+Notification.requestPermission().then(() => new Notification('Bienvenido!'))
 
 initialData = [{
     "name": "Ir al supermercado",
-    "done": false
+    "done": false,
+    "time": 100
 }, {
     "name": "Hacer mi tarea",
-    "done": true
+    "done": true,
+    "time": 120
 }, {
     "name": "Hacer tutorial WebAssembly",
-    "done": false
+    "done": false,
+    "time": 15
 }]
 
-app.addTodo = function (name, isDone) {
-    console.log(name)
+app.addTodo = function (name, isDone, time) {
     app.data.push({
         "name": name,
-        "done": isDone
+        "done": isDone,
+        "time": time
     })
     let ischecked = isDone ? "checked" : ""
     if (name != "") {
         document.querySelector("ul").insertAdjacentHTML("beforeend",
-            "<li>" +
+            "<li name = new title = "+ time +">" +
             name +
-            "<span class='right'> <input type='checkbox' " +
+            "<span class='right'> <span class='timer'></span> <input type='checkbox' " +
             ischecked +
-            "> </span> </li>")
+            "> </span></li>")
     }
     app.saveData()
+    app.refresh_time()
 }
+
+app.refresh_time = function() {
+    document.querySelectorAll("li").forEach(function(element, i){
+        console.log(i)
+        if (element.getAttribute("name") == 'new')
+        {
+            element.setAttribute("name", "used")
+        // var countDownDate = new Date(element.title + 1000 * 10 ).getTime();
+            var x = setInterval(function() {
+                element.title = element.title - 1
+                if (element.title == 0) {
+                    element.querySelector('.timer').innerHTML = "Expired";
+                    clearInterval(x)
+                    new Notification("Expiro " + app.data[i].name)
+                }
+                else
+                {
+                    var minutes = Math.floor((element.title / 60));
+                    var seconds = Math.floor((element.title % 60));
+                    element.querySelector('.timer').innerHTML = minutes.toString().padStart(2,0) + ":" + seconds.toString().padStart(2,0);
+                    app.data[i].time = element.title
+                    app.saveData()
+                }
+            }, 1000);
+        }
+    })
+    }
 
 document.getElementById('submitButton').addEventListener('click', function () {
     const tarea = document.getElementById('tareaInput')
     const nombreTarea = tarea.value
-    app.addTodo(nombreTarea, false)
+    app.addTodo(nombreTarea, false, 300)
     tarea.value = ""
 })
 
@@ -46,12 +78,12 @@ app.initialize = function () {
         console.log(JSON.parse(localStorage.data))
 
         JSON.parse(localStorage.data).forEach(element => {
-            app.addTodo(element.name, element.done)
+            app.addTodo(element.name, element.done, element.time)
         })
     } else {
         console.log("no localsotarge")
         initialData.forEach(element => {
-            app.addTodo(element.name, element.done)
+            app.addTodo(element.name, element.done, element.time)
         });
     }
 
@@ -63,3 +95,5 @@ app.saveData = function () {
 }
 
 app.initialize()
+
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-if ('serviceWorker' in navigator) {
-    console.log("SW Supported")
-    window.addEventListener('load', () => {
+if ('serviceWorker' in navigator) 
+{
+    console.log("Supported")
+    window.addEventListener('load', () => 
+    {
         navigator.serviceWorker.register("../service-worker.js")
-            .then(reg => console.log("Service Worker: Registered"))
-            .catch(err => console.log("Didnt work: ${err}"))
+        .then(reg => console.log("Service Worker: Registered"))
+        .catch(err => console.log("Didnt work: ${err}")) 
     })
 }
